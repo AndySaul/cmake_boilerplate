cf3abb3a30b1286d239d50569fc18e189d8aff56
Andy Saul <andycsaul@gmail.com>
2021-01-12 19:37:46 +0000

    Add a dummy test that uses gmock

--- Message/Message_tests.cpp
+++ Message/Message_tests.cpp
@@ -1,12 +1,47 @@
 
 #include "Message.h"
 
-#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 using namespace message;
 using namespace std::string_literals;
 
+using namespace ::testing;
+
 TEST(Message, displays_hello_world)
 {
   EXPECT_EQ("Hello, World!"s, greeting());
+}
+
+class Widget
+{
+public:
+  virtual ~Widget() = default;
+
+  virtual void doSomething() = 0;
+  virtual int add(int x, int y) const = 0;
+};
+
+class MockWidget : public Widget
+{
+public:
+  MOCK_METHOD(void, doSomething, (), (override));
+  MOCK_METHOD(int, add, (int x, int y), (const, override));
+};
+
+auto Use(Widget &widget)-> int
+{
+  widget.doSomething();
+  return widget.add(1, 2);
+}
+
+TEST(Message, calls_widget_add_when_passed_to_func)
+{
+  MockWidget widget;
+  EXPECT_CALL(widget, add(1, 2)).WillOnce(Return(42));
+  EXPECT_CALL(widget, doSomething());
+
+  auto const result = Use(widget);
+
+  EXPECT_EQ(42, result);
 }
