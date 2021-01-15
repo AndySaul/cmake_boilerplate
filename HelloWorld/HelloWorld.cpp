be1a5bd9ec8a47062f2c17ef139f118aef1c84c2
Andy Saul <andycsaul@gmail.com>
2021-01-15 10:22:54 +0000

    Use the Widget lib in HelloWorld

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,15 +1,21 @@
 #include "Message.h"
 #include "Version.h"
+#include "Gadget.h"
 
 #include <iostream>
 
 using namespace AndySaul;
+using namespace Widget;
 namespace v = version;
 
-int main(int /*argc*/, char **argv)
+int main(int /*argc*/, char** /*argv*/)
 {
   std::cout << "Version " << v::MAJOR << "." << v::MINOR << '\n';
   std::cout << message::greeting() << '\n';
+
+  std::cout << "Using the Widget lib:\n";
+  Gadget gadget;
+  std::cout << "gadget.combine(1, 2): " << gadget.combine(1, 2) << '\n';
 
   return 0;
 }
