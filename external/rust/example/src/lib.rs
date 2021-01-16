b260050e2205c3de4ce06492d51761b1739cf9b9
Andy Saul <andycsaul@gmail.com>
2021-01-16 20:30:26 +0000

    Add example Rust lib

---
+++ external/rust/example/src/lib.rs
@@ -0,0 +1,17 @@
+#[no_mangle]
+pub extern "C" fn hello_from_rust() {
+    println!("Hello from Rust")
+}
+
+#[no_mangle]
+pub extern "C" fn combine(x: u32, y: u32) -> u32 {
+    x + y
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(crate::combine(40, 2), 42);
+    }
+}
