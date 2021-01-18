e8d9b281c1d1d75d7ff231f20976b9c09ba7d73e
Andy Saul <andycsaul@gmail.com>
2021-01-18 14:18:13 +0000

    Compiles rust_lib example
    Note: Run in MSVC. MinGW needs pthread installing

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -8,6 +8,8 @@ using namespace AndySaul;
 using namespace Widget;
 namespace v = version;
 
+extern "C" void hello_from_rust();
+
 int main(int /*argc*/, char ** /*argv*/)
 {
   std::cout << "Version " << v::MAJOR << "." << v::MINOR << '\n';
@@ -16,6 +18,8 @@ int main(int /*argc*/, char ** /*argv*/)
   std::cout << "Using the Widget lib:\n";
   Gadget gadget;
   std::cout << "gadget.combine(1, 2): " << gadget.combine(1, 2) << '\n';
+
+  hello_from_rust();
 
   return 0;
 }
